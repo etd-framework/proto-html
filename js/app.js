6c4572e7bc1e6e94c3c7a206982e6a1333fc1555
Maxence Delattre <maxencedelattre62@gmail.com>
2017-05-12 17:40:43 +0200

    bump

---
+++ js/app.js
@@ -0,0 +1,10 @@
+/*!
+ * @package     etd-framework/proto-html
+ *
+ * @version     1.0.0
+ * @copyright   Copyright (C) 2017 ETD Solutions. Tous droits réservés.
+ * @license     http://www.apache.org/licenses/
+ * @author      ETD Solutions
+ */
+
+// Insert here your JS code
